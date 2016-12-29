71149045c5d7f23a26e9c8ee2ad79ec336f88372
Eamonn de Leastar <edeleastar@wit.ie>
2016-12-29 08:52:21 +0000

    port update

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ const dreams = [
 ];
 
 // listen for requests :)
-const listener = app.listen(process.env.PORT | 4000, function () {
+const listener = app.listen(process.env.PORT || 4000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
